566eb862241eb73cf8bd581dcb9e0acca18b56cb
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2013-07-23 12:53:32 -0300

    Adicionando BCrypt e mudando layout

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,9 @@
 class Room < ActiveRecord::Base
   attr_accessible :description, :location, :title
+
+  validates_presence_of :description, :location, :title
+
+  def complete_name
+  	"#{title}, #{location}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ColchoNet::Application.routes.draw do
   resources :users
 
   root to: 'home#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
